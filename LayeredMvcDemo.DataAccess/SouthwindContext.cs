e1bf3674ec963122a9cf9451ce1fd3874a562742
chuangmaster <chuang.master@gmail.com>
2018-08-26 01:52:08 +0800

    Add Data Access Layer

---
+++ LayeredMvcDemo.DataAccess/SouthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LayeredMvcDemo.Domain.Models;
+
+namespace LayeredMvcDemo.DataAccess
+{
+    public class SouthwindContext : DbContext
+    {
+        public SouthwindContext() : base("SouthwindDB")
+        {
+            Database.SetInitializer<SouthwindContext>(new SouthwindDBInitializer());
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }   
+}
---
+++ LayeredMvcDemo.DataAccess/SouthwindDBInitializer.cs
@@ -0,0 +1,45 @@
+﻿using LayeredMvcDemo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredMvcDemo.DataAccess
+{
+    public class SouthwindDBInitializer : CreateDatabaseIfNotExists<SouthwindContext>
+    {
+        public override void InitializeDatabase(SouthwindContext context)
+        {
+            base.InitializeDatabase(context);
+
+            context.Customers.Add(new Customer
+            {
+                Id = 1,
+                CompanyName = "MikeSoft",
+                Contact = "Michael"
+            });
+            context.Customers.Add(new Customer
+            {
+                Id = 2,
+                CompanyName = "OralCall",
+                Contact = "Vivid"
+            });
+            context.Customers.Add(new Customer
+            {
+                Id = 3,
+                CompanyName = "SimonTech",
+                Contact = "Simon"
+            });
+            context.Customers.Add(new Customer
+            {
+                Id = 4,
+                CompanyName = "IoSee",
+                Contact = "Mark"
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
