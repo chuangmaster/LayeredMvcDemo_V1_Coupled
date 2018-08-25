9ffbc83e0f706f1deb6831a1452cb2bf6a0542d6
chuangmaster <chuang.master@gmail.com>
2018-08-26 01:46:04 +0800

    加入專案檔案。

---
+++ LayeredMvcDemo_V1_Coupled/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LayeredMvcDemo_V1_Coupled
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LayeredMvcDemo_V1_Coupled/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LayeredMvcDemo_V1_Coupled.MvcApplication" Language="C#" %>
